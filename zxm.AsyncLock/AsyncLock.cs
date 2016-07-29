6cfae4d682f6ec3cefdd492c35018ccd0e237f60
zxm <hhappyzxm@163.com>
2016-07-29 14:04:32 +0800

    Init project

---
+++ zxm.AsyncLock/AsyncLock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace zxm.AsyncLock
+{
+    public class AsyncLock
+    {
+        private readonly AsyncSemaphore _semaphore;
+        private readonly Task<Releaser> _releaser;
+
+        public AsyncLock()
+        {
+            _semaphore = new AsyncSemaphore(1);
+            _releaser = Task.FromResult(new Releaser(this));
+        }
+
+        public Task<Releaser> LockAsync()
+        {
+            var wait = _semaphore.WaitAsync();
+            return wait.IsCompleted ?
+                _releaser :
+                wait.ContinueWith((_, state) => new Releaser((AsyncLock)state),
+                    this, CancellationToken.None,
+                    TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+        }
+
+        public struct Releaser : IDisposable
+        {
+            private readonly AsyncLock m_toRelease;
+
+            internal Releaser(AsyncLock toRelease) { m_toRelease = toRelease; }
+
+            public void Dispose()
+            {
+                m_toRelease?._semaphore.Release();
+            }
+        }
+    }
+}
